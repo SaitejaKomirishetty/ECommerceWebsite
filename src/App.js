f66db652d8327ff1e2e71e015ea46c109595a574
SAITEJA KOMIRISHETTY <komirishettysaiteja@gmail.com>
2024-10-29 17:33:21 +0530

    Fixed routing issue

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
-import React from 'react'
-import { BrowserRouter, Switch, Route } from 'react-router-dom'
-import { Navbar, Sidebar, Footer } from './components'
+import React from 'react';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import { Navbar, Sidebar, Footer } from './components';
 import {
     Home,
     Products,
@@ -9,46 +9,45 @@ import {
     Cart,
     Error,
     Checkout,
-    AuthWrapper
-} from "./pages";
+    AuthWrapper,
+} from './pages';
 import PrivateRoute from './pages/PrivateRoute';
 
-
 function App() {
-  return (
-      <AuthWrapper>
-          <BrowserRouter>
-              <Navbar />
-              <Sidebar />
-              <Switch>
-                  <Route exact path="/">
-                      <Home />
-                  </Route>
-                  <Route exact path="/about">
-                      <About />
-                  </Route>
-                  <Route exact path="/cart">
-                      <Cart />
-                  </Route>
-                  <Route exact path="/products">
-                      <Products />
-                  </Route>
-                  <Route
-                      exact
-                      path="/products/:id"
-                      children={<SingleProduct />}
-                  ></Route>
-                  <PrivateRoute exact path="/checkout">
-                      <Checkout />
-                  </PrivateRoute>
-                  <Route exact path="*">
-                      <Error />
-                  </Route>
-              </Switch>
-              <Footer />
-          </BrowserRouter>
-      </AuthWrapper>
-  );
+    return (
+        <AuthWrapper>
+            <BrowserRouter basename='/ECommerceWebsite'>
+                <Navbar />
+                <Sidebar />
+                <Switch>
+                    <Route exact path='/'>
+                        <Home />
+                    </Route>
+                    <Route exact path='/about'>
+                        <About />
+                    </Route>
+                    <Route exact path='/cart'>
+                        <Cart />
+                    </Route>
+                    <Route exact path='/products'>
+                        <Products />
+                    </Route>
+                    <Route
+                        exact
+                        path='/products/:id'
+                        children={<SingleProduct />}
+                    ></Route>
+                    <PrivateRoute exact path='/checkout'>
+                        <Checkout />
+                    </PrivateRoute>
+                    <Route exact path='*'>
+                        <Error />
+                    </Route>
+                </Switch>
+                <Footer />
+            </BrowserRouter>
+        </AuthWrapper>
+    );
 }
 
-export default App
+export default App;
